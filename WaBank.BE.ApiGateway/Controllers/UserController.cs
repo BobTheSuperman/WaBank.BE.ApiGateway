ee159654e24f9d89bc7b46410095a947339af40a
DESKTOP-ATG2IPI\serge <sergey.babenko.boss@gmail.com>
2023-09-27 13:06:34 +0300

    created user controller

---
+++ WaBank.BE.ApiGateway/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WaBank.BE.ApiGateway.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<ActionResult> Login(/*some model here*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Task<ActionResult> LogOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<ActionResult> Register(/*model*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<ActionResult> RegisterConfirmed(/*model*/)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
